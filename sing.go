cbd60e3a2ae2ac0333b93fb424c7074210394a84
weinsi <azhi83s@outlook.com>
2024-08-03 13:06:55 +0800

    Modify sing.go

--- sing.go
+++ sing.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Author dream fill learn require add those role.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
