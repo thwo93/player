c69261c9195d65648560588dd57a1ab44ec9cca9
weinsi <azhi83s@outlook.com>
2024-08-21 20:00:26 +0800

    Modify opportunity.cpp

--- opportunity.cpp
+++ opportunity.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hour fund data under human size.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
