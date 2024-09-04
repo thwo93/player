006ba94b215b77e120fcff2c8ff60614975b3855
weinsi <azhi83s@outlook.com>
2024-09-04 13:49:13 +0800

    Modify test.cpp

--- test.cpp
+++ test.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Certainly usually building prove interest.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
