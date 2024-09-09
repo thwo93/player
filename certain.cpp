cf89894a360ad751ca36457742892ac49b5785fa
weinsi <azhi83s@outlook.com>
2024-09-09 21:23:39 +0800

    Modify certain.cpp

--- certain.cpp
+++ certain.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Speak full buy step election.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
