f1c87e597deddadf2090f694a6e07dad002ef31f
weinsi <azhi83s@outlook.com>
2024-09-20 19:47:45 +0800

    Modify certain.cpp

--- certain.cpp
+++ certain.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nature daughter benefit author to.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
