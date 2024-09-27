51dd2f6f91fd7a2d76483c743e187b3ea5dd0ecc
weinsi <azhi83s@outlook.com>
2024-09-27 12:49:51 +0800

    Modify test.cpp

--- test.cpp
+++ test.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Improve compare nothing situation direction.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
