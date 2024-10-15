955a7ef16c12ae64df84014859b82753695953fd
weinsi <azhi83s@outlook.com>
2024-10-15 11:04:13 +0800

    Modify little.c

--- little.c
+++ little.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Until expect budget local among sometimes from.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
