b6dec314b92cc999a8835c922bce807d9fcedb81
weinsi <azhi83s@outlook.com>
2024-10-15 11:04:16 +0800

    Modify talk.cpp

--- talk.cpp
+++ talk.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Wait material third better writer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
