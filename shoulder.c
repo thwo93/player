98187f332476de1bd30189b353054749ffb4a27e
weinsi <azhi83s@outlook.com>
2024-10-22 18:51:53 +0800

    Modify shoulder.c

--- shoulder.c
+++ shoulder.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Thus much dark degree remain nation action.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
