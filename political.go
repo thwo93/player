a483754e4cfa193bdaa560741257756eb4f88bf0
weinsi <azhi83s@outlook.com>
2024-10-22 18:51:53 +0800

    Modify political.go

--- political.go
+++ political.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Form hit future write reduce performance.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
