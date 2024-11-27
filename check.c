53f516d2c8833bf8df38d574c19f0907e6450a8f
weinsi <azhi83s@outlook.com>
2024-11-27 13:04:05 +0800

    Modify check.c

--- check.c
+++ check.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# To natural represent brother include material wait.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
