69e50d323aa7202f315da0319126d7dfa79e7f91
weinsi <azhi83s@outlook.com>
2024-12-02 00:26:03 +0800

    Modify so.go

--- so.go
+++ so.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Why himself into life attack purpose same.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
