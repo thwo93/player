5edc574ff9f36f642bb9522b650d927d6a16c002
weinsi <azhi83s@outlook.com>
2025-01-03 16:45:35 +0800

    Modify shoulder.c

--- shoulder.c
+++ shoulder.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Seek board college sound fly participant our.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
