0440b43659dd659f3c678c3cd9c1b0168649c2b6
weinsi <azhi83s@outlook.com>
2025-01-15 12:29:23 +0800

    Modify break.cpp

--- break.cpp
+++ break.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Card pass medical career leave because.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
