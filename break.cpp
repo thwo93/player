bf3b929738ea41bca5ad2e96d5e9e357e6d03bfd
weinsi <azhi83s@outlook.com>
2025-01-15 12:29:23 +0800

    Modify break.cpp

--- break.cpp
+++ break.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Why woman leave beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
